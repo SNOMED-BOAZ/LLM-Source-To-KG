6f34c4e3fd41d4c70ea99efba1f8db8dbb60706f
g-hyeong <rjsgud5386@gmail.com>
2025-05-28 18:57:51 +0900

    Merge with master

--- src/llm_source_to_kg/graph/cohort_graph/nodes/extract_cohorts.py
+++ src/llm_source_to_kg/graph/cohort_graph/nodes/extract_cohorts.py
@@ -4,9 +4,6 @@ from llm_source_to_kg.utils.llm_util import get_llm
 from llm_source_to_kg.utils.logger import get_logger
 from llm_source_to_kg.schema.llm import LLMMessage, LLMConfig
 from json_repair import repair_json
-import os
-
-PROJECT_ROOT = "/Users/hyejiyu/Desktop/2025/BOAZ_ADV/langGraph/BOAZ-SNUH_llm_source_to_kg"
 
 async def extract_cohorts(state: CohortGraphState) -> CohortGraphState:
     """
@@ -21,9 +18,7 @@ async def extract_cohorts(state: CohortGraphState) -> CohortGraphState:
         top_p=0.95,
         max_output_tokens=8192
     )
-    prompt_file_path = os.path.join(PROJECT_ROOT, "src/llm_source_to_kg/graph/cohort_graph/prompts/extract_cohort_prompt.txt")
-
-    prompt = open(prompt_file_path, "r").read()
+    prompt = open("../prompts/extract_cohort_prompt.txt", "r").read()
 
     messages = [
         LLMMessage(role="system", content=prompt),
@@ -42,10 +37,9 @@ async def extract_cohorts(state: CohortGraphState) -> CohortGraphState:
 
 # 테스트용 코드
 if __name__ == "__main__":
-    file_path = os.path.join(PROJECT_ROOT, "datasets/guideline/contents/NG238.json")
 
     state = CohortGraphState(
-        document=open(file_path, "r").read(),
+        document=open("../../../../../datasets/guideline/contents/NG238.json", "r").read(),
         source_reference_number="NG238",
         cohort_result=[]
     )
